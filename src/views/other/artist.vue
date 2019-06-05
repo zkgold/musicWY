7cdbadaa8324c24cc3e6c715856125b024b3b5fa
zkgold <1446103090@qq.com>
2019-06-05 22:05:49 +0800

    solve mistake one

--- src/views/other/artist.vue
+++ src/views/other/artist.vue
@@ -1,11 +1,22 @@
 <template>
 	<div id="musciApp">
-		
+		<div class="wrap">
+			<div class="box_left">
+				nothing
+			</div>
+			<div class="box_right">
+				<div class="right">nothing</div>
+			</div>
+		</div>
+		<div class="footer">
+			<video :src="musicUrl"></video>
+			宇宙之大 我在其中
+		</div>
 	</div>
 </template>
 <script>
 export default {
-
+	name: 'musicApp',
 }
 </script>
 <style lang='scss' scoped>
